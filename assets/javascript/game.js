5a8303997d369514a6cf5b2c245a1dd88d25dfb0
mlebjorke <mlestraus@gmail.com>
2018-11-14 22:57:32 -0700

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,11 @@
 var losses = 0;
 var wins = 0;
-var computerNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+var computerNumber = makeRandCompNumber()
 var userTotal = 0;
+
+function makeRandCompNumber () {
+    return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+}
 
 var crystal=[
     {name: "blue",
@@ -21,12 +25,53 @@ var crystal=[
             score: 0
     }                  
 ]
-
-$("#computer-number").html(computerNumber)
 crystal.forEach(element => {
-    console.log(element);
+    // console.log(element);
     var rand = Math.floor(Math.random() * 12) + 1;
-    $('#crystals').append(`<img src="${element.img}" data-points="${rand}">`)
+    $('#crystals').append(`<img class="crystal" src="${element.img}" data-points="${rand}">`)
 });
 
-$("img").click()
+
+$("#computer-number").html(computerNumber)
+function resetCrystals(){
+    var resetCrystals = $(".crystal").get()
+    // console.log(resetCrystals);
+    
+    resetCrystals.forEach(function(element){
+        var rand = Math.floor(Math.random() * 12) + 1;
+        // console.log(element);
+
+        element.setAttribute("data-points", rand)
+    })
+}
+$(".crystal").on("click", function() {
+    console.log(this);
+    console.log(userTotal);
+    console.log(computerNumber);
+    
+    var crystalValue = parseInt($(this).attr("data-points"));
+    userTotal = userTotal + crystalValue;
+    if(userTotal < computerNumber) {
+        $("#total").html(userTotal)
+    } else if(userTotal === computerNumber){
+        wins++
+        $("#wins").html(wins)
+        computerNumber = makeRandCompNumber()
+        $("#computer-number").html(computerNumber)
+        $("#total").html(0)
+        userTotal = 0
+        resetCrystals()
+
+        // alert("You won!")
+    } else {
+        losses++
+        $("#losses").html(losses)
+        computerNumber = makeRandCompNumber()
+        $("#computer-number").html(computerNumber)
+        $("#total").html(0)
+        userTotal = 0
+        resetCrystals()
+        // alert("You lost!")
+    }
+})
+
